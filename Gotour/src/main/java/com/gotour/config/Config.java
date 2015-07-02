e47e5749e26bbc0b9412b31c0d8853f74624271b
zem3 <zemm10a@gmail.com>
2015-07-02 19:48:08 +0100

    Spring MVC configured

--- Gotour/src/main/java/com/gotour/config/Config.java
+++ Gotour/src/main/java/com/gotour/config/Config.java
@@ -1,8 +1,10 @@
 package com.gotour.config;
 
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
@@ -10,21 +12,28 @@ import org.springframework.web.servlet.view.JstlView;
 import org.springframework.web.servlet.view.UrlBasedViewResolver;
 
 @Configuration
-@ComponentScan("com.gotour")
+@ComponentScan(basePackages = "com.gotour")
 @EnableWebMvc
 public class Config extends WebMvcConfigurerAdapter {
 
-  @Bean
-  public UrlBasedViewResolver setupViewResolver() {
-    UrlBasedViewResolver resolver = new UrlBasedViewResolver();
-    resolver.setPrefix("/WEB-INF/jsp/");
-    resolver.setSuffix(".jsp");
-    resolver.setViewClass(JstlView.class);
-    return resolver;
-  }
+    @Bean
+    public UrlBasedViewResolver setupViewResolver() {
+        UrlBasedViewResolver resolver = new UrlBasedViewResolver();
+        resolver.setPrefix("/WEB-INF/jsp/");
+        resolver.setSuffix(".jsp");
+        resolver.setViewClass(JstlView.class);
+        return resolver;
+    }
 
-  @Override
-  public void addResourceHandlers(ResourceHandlerRegistry registry) {
-    registry.addResourceHandler("/resources/**").addResourceLocations("/WEB-INF/resources/*");
-  }
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/resources/**").addResourceLocations("/WEB-INF/resources/*");
+    }
+
+    @Bean
+    public MessageSource messageSource() {
+        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+        messageSource.setBasename("messages");
+        return messageSource;
+    }
 }
