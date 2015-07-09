022ebde8341647d728d481a594d879fe22745774
Miguel Pinto <mcpinto98@gmail.com>
2015-07-09 17:00:37 +0100

    users profile

--- Gotour/src/main/java/com/gotour/controllers/UsersController.java
+++ Gotour/src/main/java/com/gotour/controllers/UsersController.java
@@ -1,41 +1,45 @@
 package com.gotour.controllers;
 
-import com.gotour.models.Tourist;
 import com.gotour.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/users")
 public class UsersController {
 
-    @Autowired
-    UserService service;
-    
-    @RequestMapping(value = "/signup", method = RequestMethod.GET)
-    public String signup(ModelMap map) {
-        map.put("msg", "Hello World!");
-        return "user/signup";
-    }
+  @Autowired
+  UserService userService;
 
-    @RequestMapping(value = "/new", method = RequestMethod.POST)
-    public String register(ModelMap model) {       
-        return "";
-    }
+  @RequestMapping(value = "/signup", method = RequestMethod.GET)
+  public String signup(ModelMap map) {
+    map.put("msg", "Hello World!");
+    return "user/signup";
+  }
 
-    @RequestMapping(value = "/login", method = RequestMethod.GET)
-    public String login(ModelMap map) {
-        map.put("msg", "Hello World!");
-        return "user/login";
-    }
+  @RequestMapping(value = "/new", method = RequestMethod.POST)
+  public String register(ModelMap model) {
+    return "";
+  }
 
-    @RequestMapping(value = "/signin", method = RequestMethod.POST)
-    public String signin(ModelMap map) {
+  @RequestMapping(value = "/login", method = RequestMethod.GET)
+  public String login(ModelMap map) {
+    map.put("msg", "Hello World!");
+    return "user/login";
+  }
 
-        return "";
-    }
+  @RequestMapping(value = "/signin", method = RequestMethod.POST)
+  public String signin(ModelMap map) {
+
+    return "";
+  }
+
+  @RequestMapping(value = "/profile", method = RequestMethod.GET)
+  public String profile(ModelMap map) {
+
+    return "user/profile";
+  }
 }
