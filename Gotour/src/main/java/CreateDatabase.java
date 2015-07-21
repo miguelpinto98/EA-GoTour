b48d18aecb6961e33bde7cbbda4c06b39ecbb0bb
zem3 <zemm10a@gmail.com>
2015-07-21 07:54:00 +0100

    adds avatar path for all users

--- Gotour/src/main/java/CreateDatabase.java
+++ Gotour/src/main/java/CreateDatabase.java
@@ -172,12 +172,12 @@ public class CreateDatabase {
       t.setName(user);
       t.setEmail(user + "@gotour.com");
       t.setPassword("gotour");
-      t.setAvatar(i + "_" + user);
+      t.setAvatar(i + "_" + user + ".jpg");
       t.setDescription(faker.lorem().sentence());
       us.addTourist(t);
       i++;
     }
-
+    
     Name p = faker.name();
     String name;
     for (int j = 1; j <= TOURISTS; j++) {
@@ -187,6 +187,7 @@ public class CreateDatabase {
       t.setEmail(name.toLowerCase() + "@gotour.com");
       t.setPassword("gotour");
       t.setDescription(faker.lorem().sentence());
+      t.setAvatar(((j % tourists.length) + 1) + "_" + tourists[j%tourists.length] + ".jpg");
       us.addTourist(t);
     }
 
@@ -198,7 +199,7 @@ public class CreateDatabase {
       g.setPassword("gotour");
       g.setPhone(faker.phoneNumber().phoneNumber());
       g.setDescription(faker.lorem().paragraph(2));
-      g.setAvatar(i + "_" + user);
+      g.setAvatar(i + "_" + user + ".jpg");
       us.addGuide(g);
       i++;
     }
@@ -211,6 +212,7 @@ public class CreateDatabase {
       g.setPassword("gotour");
       g.setPhone(faker.phoneNumber().phoneNumber());
       g.setDescription(faker.lorem().paragraph(2));
+      g.setAvatar(((k % guides.length) + 1) + "_" + guides[k%guides.length] + ".jpg");
       us.addGuide(g);
     }
 
@@ -221,6 +223,7 @@ public class CreateDatabase {
     g.setPassword("gotour");
     g.setPhone("929292929");
     g.setDescription("");
+    g.setAvatar("pintinho.jpg");
     us.addGuide(g);
   }
 
